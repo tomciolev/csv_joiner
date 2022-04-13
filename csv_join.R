452ecd28663e12b0bf840ee76fb148da97d55664
solarskit@student.agh.edu.pl <solarskit@student.agh.edu.pl>
2022-04-13 10:20:50 +0200

    left join function

--- csv_join.R
+++ csv_join.R
@@ -30,19 +30,28 @@ col_name <- "employee_id"
 joined <- data.frame(matrix(ncol = ncol(df1)+ncol(df2), nrow = 0))
 colnames(joined) <- c(colnames(df1), colnames(df2))
 
-for(i in 1:nrow(df1)){
-  if(is.na(df1[i,col_name])){
-    joined[k,] <- c(df1[i,], NA,NA,NA)
-    k = k+1
-  }
-  else{
-    for(j in 1:nrow(df2)){
-      if(!is.na(df1[i, col_name]) && !is.na(df2[j,col_name]) && df1[i,col_name] == df2[j,col_name]){
-        joined[k,] <- c(df1[i,], df2[j,])
-        k = k+1
+
+
+left_join <- function(df1, df2, col_name){
+  joined <- data.frame(matrix(ncol = ncol(df1)+ncol(df2), nrow = 0))
+  colnames(joined) <- c(colnames(df1), colnames(df2))
+  k = 1
+  for(i in 1:nrow(df1)){
+    if(is.na(df1[i,col_name]) || !df1[i,col_name] %in% df2[,col_name]){
+      joined[k,] <- c(df1[i,], NA,NA,NA)
+      k = k+1
+    }
+    else{
+      for(j in 1:nrow(df2)){
+        if(!is.na(df1[i, col_name]) && !is.na(df2[j,col_name]) && df1[i,col_name] == df2[j,col_name]){
+          joined[k,] <- c(df1[i,], df2[j,])
+          k = k+1
+        }
       }
     }
+    
   }
-  
+  joined
 }
-
+left_join(df1,df2,"department_id")
+left_join(df2,df1,"department_id")
